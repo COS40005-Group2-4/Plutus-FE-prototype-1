ffa6f1880d21ff80d88940963074358d146ef639
saviorab1 <popww147@gmail.com>
2026-01-22 16:20:49 +0700

    Add amplify config file

--- lib/config/google_oauth_config.dart
+++ lib/config/google_oauth_config.dart
@@ -1,15 +1,23 @@
 class GoogleOAuthConfig {
   // Client ID
   static const String clientId = '611876522758-30av79i8clflnriv7ee5dp68qft0ja28.apps.googleusercontent.com';
-  const String AMPLIFY_REDIRECT_URL = 'https://main.d3adjr6i7jedcz.amplifyapp.com';
-  // Client Secret
-  static const String clientSecret = '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';
+  
+  // Production redirect URL (Amplify)
+  static const String redirectUrlProduction = 'https://main.d3adjr6i7jedcz.amplifyapp.com';
+  
+  // Development redirect URL (local)
+  static const String redirectUrlDev = 'http://localhost:8080';
+  
+  // Use production URL for web builds, dev URL for testing
+  static const String redirectUrl = String.fromEnvironment(
+    'REDIRECT_URL',
+    defaultValue: 'http://localhost:8080',
+  );
   
   // OAuth endpoints
   static const String authorizationEndpoint =
       'https://accounts.google.com/o/oauth2/v2/auth';
   static const String tokenEndpoint = 'https://oauth2.googleapis.com/token';
-  static const String redirectUrl = 'http://localhost:8080';
   
   // Scopes
   static const List<String> scopes = [
