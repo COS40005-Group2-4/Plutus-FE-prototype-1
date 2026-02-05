631cc337e8e779d7587bbfe581933bb5cf3b628e
saviorab1 <popww147@gmail.com>
2026-02-05 20:10:13 +0700

    Update yml file for web build

--- lib/config/aws_config.dart
+++ lib/config/aws_config.dart
@@ -2,34 +2,34 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class AWSConfig {
   static String get accessKeyId =>
-      dotenv.env['AWS_ACCESS_KEY_ID'] ??
+      dotenv.env['PLUTUS_AWS_ACCESS_KEY_ID'] ??
       const String.fromEnvironment(
-        'AWS_ACCESS_KEY_ID',
+        'PLUTUS_AWS_ACCESS_KEY_ID',
         defaultValue: 'YOUR_ACCESS_KEY_ID',
       );
 
   static String get secretAccessKey =>
-      dotenv.env['AWS_SECRET_ACCESS_KEY'] ??
+      dotenv.env['PLUTUS_AWS_SECRET_ACCESS_KEY'] ??
       const String.fromEnvironment(
-        'AWS_SECRET_ACCESS_KEY',
+        'PLUTUS_AWS_SECRET_ACCESS_KEY',
         defaultValue: 'YOUR_SECRET_ACCESS_KEY',
       );
   
   static String? get sessionToken =>
-      dotenv.env['AWS_SESSION_TOKEN'] ??
-      const String.fromEnvironment('AWS_SESSION_TOKEN').ifEmpty(null);
+      dotenv.env['PLUTUS_AWS_SESSION_TOKEN'] ??
+      const String.fromEnvironment('PLUTUS_AWS_SESSION_TOKEN').ifEmpty(null);
 
   static String get region =>
-      dotenv.env['AWS_REGION'] ??
+      dotenv.env['PLUTUS_AWS_REGION'] ??
       const String.fromEnvironment(
-        'AWS_REGION',
+        'PLUTUS_AWS_REGION',
         defaultValue: 'us-east-1',
       );
 
   static String get bucketName =>
-      dotenv.env['AWS_S3_BUCKET_NAME'] ??
+      dotenv.env['PLUTUS_AWS_S3_BUCKET_NAME'] ??
       const String.fromEnvironment(
-        'AWS_S3_BUCKET_NAME',
+        'PLUTUS_AWS_S3_BUCKET_NAME',
         defaultValue: 'your-s3-bucket-name',
       );
 }
