1fc51428f1d0943881a1a46fc59b8650289c2ce1
aidendinh <anh.dinh@katalon.com>
2026-04-10 11:06:34 +0700

    chore: update Android app identity to com.plutus.app / Plutus

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.plutus_fe_prototype"
+    namespace = "com.plutus.app"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -26,7 +26,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.plutus_fe_prototype"
+        applicationId = "com.plutus.app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
