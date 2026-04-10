b4b2383e5fff37f3f7ab3516b205ec11eebf488e
aidendinh <anh.dinh@katalon.com>
2026-04-10 16:03:01 +0700

    Feat: apk export

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,14 +1,15 @@
+import java.util.Properties
+
 plugins {
     id("com.android.application")
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
-    id("com.google.gms.google-services")
 }
 
 // Load signing properties
 val keyPropertiesFile = rootProject.file("app/key.properties")
-val keyProperties = java.util.Properties()
+val keyProperties = Properties()
 if (keyPropertiesFile.exists()) {
     keyProperties.load(keyPropertiesFile.inputStream())
 }
